c4a140708a509b0bfd49369af899756a07f918eb
Lucas Nóbrega <lucas.nobrega@cjr.org.br>
2020-05-30 17:08:58 -0300

    Adicionado Register sem CSS

--- pokedex_react/src/pages/Register.js
+++ pokedex_react/src/pages/Register.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { navigate } from "@reach/router";
+
+
+/* 
+axios
+    .get("https://pokedex20201.herokuapp.com/users/" + user)
+    .then(res => {console.log(res.data.user.username + " -- OK")}
+);
+*/
+
+
+const Form = () => {
+    const [user, setUser] = useState("");
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        axios({
+            method: 'post',
+            url: 'https://pokedex20201.herokuapp.com/users',
+            data: {
+                username: user
+            }
+        })
+        .then(() => alert("Usuario criado com sucesso!"))
+        .catch(res => {console.log(res); alert("Ops deu erro!");});
+        
+        navigate('./login');
+    };
+
+    return (
+        <form onSubmit={e => submitForm(e)}>
+            <input type="text" placeholder="Digite seu nome de usuário" onChange={(e) => setUser(e.target.value)} />
+            <button type="submit">Criar Usuário</button>
+        </form>
+    );
+};
 
 const Register = () => {
-    return(
-        <div className = "register">
+    return (
+        <div className="register">
             <h1>Registrar-se</h1>
+            <Form />
         </div>
     )
 }
